1ef37f86f91d99de8a3ac118376d0a7e6ba65041
Eric Dattore <eric.dattore@gmail.com>
2022-03-21 12:11:57 -0600

    begin rewrite to installer struct

--- orb-scripts/src/bin/install-nix.rs
+++ orb-scripts/src/bin/install-nix.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use xshell::{cmd, Shell};
+use xshell::{cmd, Shell, TempDir};
 
 #[derive(Debug, PartialEq)]
 pub enum OperatingSystem {
@@ -8,40 +8,110 @@ pub enum OperatingSystem {
     MacOS,
 }
 
+// TODO: Consider rewriting this to use typestates
+// Rationale: The workdir has to be created to do certain actions, so encapsulating that
+// in the type system would protected against misuse
+// Counter-point: This script won't be shared beyond this orb, so misuse would be a concern for myself and myself only
+struct NixInstaller {
+    shell: Shell,
+    workdir: Option<TempDir>,
+}
+
+impl NixInstaller {
+    /// Create a new Nix installer
+    fn new() -> Result<Self, anyhow::Error> {
+        let shell = Shell::new()?;
+        Ok(NixInstaller {
+            shell,
+            workdir: None,
+        })
+    }
+
+    /// Check if nix is already installed; returns Err is it is
+    fn nix_not_installed(&self) -> Result<(), anyhow::Error> {
+        if cmd!(self.shell, "type -p nix").quiet().run().is_ok() {
+            println!("Nix is already installed; skipping installation");
+            return Err(anyhow::anyhow!(
+                "Nix is present on the system; skipping installation"
+            ));
+        }
+        Ok(())
+    }
+
+    /// Create tempdir
+    fn create_workdir(&mut self) -> Result<(), anyhow::Error> {
+        let tempdir = self.shell.create_temp_dir()?;
+        self.workdir = Some(tempdir);
+        Ok(())
+    }
+
+    /// Write config file
+    fn write_config(&self) -> Result<(), anyhow::Error> {
+        let extra_nix_config = self
+            .shell
+            .var("EXTRA_NIX_CONFIG")
+            .unwrap_or_else(|_| "".to_string());
+        let mut config = vec![
+            "max-jobs = auto",
+            "trust-users = root $USER",
+            &extra_nix_config,
+        ];
+
+        if !extra_nix_config.contains("experimental-features") {
+            config.push("experimental-features = nix-command flakes");
+        }
+
+        let file = self
+            .workdir
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("workdir has not been created"))?
+            .path()
+            .join("nix.conf");
+
+        self.shell.write_file(file, config.join("\n"))?;
+
+        Ok(())
+    }
+
+    /// Assemble installer flags
+    fn assemble_installer_flags(&self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
+
+    /// Download the installer
+    fn download_installer(&self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
+
+    /// Run the installer
+    fn run_installer(&self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
+
+    /// Returns a reference to the held `Shell`
+    fn shell(&self) -> &Shell {
+        &self.shell
+    }
+
+    /// Returns a reference to the held `TempDir`
+    fn workdir(&self) -> &Option<TempDir> {
+        &self.workdir
+    }
+}
+
 fn main() -> Result<(), anyhow::Error> {
+    let mut installer = NixInstaller::new()?;
     let sh = Shell::new()?;
 
-    // Check if Nix is already installed
-    if cmd!(sh, "type -p nix").quiet().run().is_ok() {
-        println!("Nix is already installed; skipping installation");
-        return Ok(());
-    }
+    installer.nix_not_installed()?;
+
+    installer.create_workdir()?;
 
     // Create a tempdir
     // Write config file
     // Assemble installer flags
     // Download the installer
     // Run the installer
-
-    let workdir = sh.create_temp_dir()?;
-    let mut config = vec![
-        "max-jobs = auto",
-        "trust-users = root $USER",
-        "$EXTRA_NIX_CONFIG",
-    ];
-
-    if cmd!(
-        sh,
-        "[ ! $INPUT_EXTRA_NIX_CONFIG =~ \"experimental-features\" ]"
-    )
-    .run()
-    .is_ok()
-    {
-        config.push("experimental-features = nix-command flakes");
-    }
-
-    let file = workdir.path().join("nix.conf");
-    write_config(&sh, &file, &config.join("\n"))?;
 
     let _ = detect_os(&sh)?;
 
@@ -60,25 +130,19 @@ fn detect_os(sh: &Shell) -> Result<OperatingSystem, anyhow::Error> {
     }
 }
 
-fn write_config(sh: &Shell, file: &Path, contents: &str) -> Result<(), anyhow::Error> {
-    sh.write_file(file, contents)?;
-
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn setup() -> Shell {
-        Shell::new().unwrap()
+    fn setup() -> NixInstaller {
+        NixInstaller::new().unwrap()
     }
 
     #[cfg(target_os = "macos")]
     #[test]
     fn identifies_os_as_macos() {
-        let sh = setup();
-        let os = detect_os(&sh);
+        let installer = setup();
+        let os = detect_os(installer.shell());
         assert!(os.is_ok());
         assert_eq!(os.unwrap(), OperatingSystem::MacOS);
     }
@@ -86,23 +150,27 @@ mod tests {
     #[cfg(target_os = "linux")]
     #[test]
     fn identifies_os_as_linux() {
-        let sh = setup();
-        let os = detect_os(&sh);
+        let installer = setup();
+        let os = detect_os(installer.shell());
         assert!(os.is_ok());
         assert_eq!(os.unwrap(), OperatingSystem::Linux);
     }
 
     #[test]
     fn writes_config_file_correctly() {
-        let sh = setup();
-        let tempdir = sh.create_temp_dir().unwrap();
-        let contents = "max-jobs = 4";
+        let mut installer = setup();
+        installer.create_workdir().unwrap();
+        let file = installer
+            .workdir()
+            .as_ref()
+            .unwrap()
+            .path()
+            .join("nix.conf");
 
-        let file = tempdir.path().join("nix.conf");
-        write_config(&sh, &file, contents).unwrap();
+        installer.write_config().unwrap();
 
-        let written_contents = sh.read_file(file).unwrap();
-
-        assert_eq!(contents, written_contents);
+        let written_contents = installer.shell().read_file(file).unwrap();
+        assert!(!written_contents.is_empty());
+        assert!(written_contents.contains("max-jobs = auto"));
     }
 }
