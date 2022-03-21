5f1c35ba28d671aded19f43aa3d94a4eebce7080
Eric Dattore <eric.dattore@gmail.com>
2022-03-21 15:19:06 -0600

    finish script

--- orb-scripts/src/bin/install-nix.rs
+++ orb-scripts/src/bin/install-nix.rs
@@ -1,10 +1,9 @@
-use std::path::Path;
-
 use xshell::{cmd, Shell, TempDir};
 
 #[derive(Debug, PartialEq)]
 pub enum OperatingSystem {
     Linux,
+    LinuxSystemD,
     MacOS,
 }
 
@@ -15,20 +14,25 @@ pub enum OperatingSystem {
 struct NixInstaller {
     shell: Shell,
     workdir: Option<TempDir>,
+    os: OperatingSystem,
+    installer_options: Vec<String>,
 }
 
 impl NixInstaller {
     /// Create a new Nix installer
-    fn new() -> Result<Self, anyhow::Error> {
+    pub fn new() -> Result<Self, anyhow::Error> {
         let shell = Shell::new()?;
+        let os = detect_os(&shell)?;
         Ok(NixInstaller {
             shell,
             workdir: None,
+            os,
+            installer_options: vec![],
         })
     }
 
     /// Check if nix is already installed; returns Err is it is
-    fn nix_not_installed(&self) -> Result<(), anyhow::Error> {
+    pub fn nix_not_installed(&self) -> Result<(), anyhow::Error> {
         if cmd!(self.shell, "type -p nix").quiet().run().is_ok() {
             println!("Nix is already installed; skipping installation");
             return Err(anyhow::anyhow!(
@@ -39,14 +43,14 @@ impl NixInstaller {
     }
 
     /// Create tempdir
-    fn create_workdir(&mut self) -> Result<(), anyhow::Error> {
+    pub fn create_workdir(&mut self) -> Result<(), anyhow::Error> {
         let tempdir = self.shell.create_temp_dir()?;
         self.workdir = Some(tempdir);
         Ok(())
     }
 
     /// Write config file
-    fn write_config(&self) -> Result<(), anyhow::Error> {
+    pub fn write_config(&self) -> Result<(), anyhow::Error> {
         let extra_nix_config = self
             .shell
             .var("EXTRA_NIX_CONFIG")
@@ -59,6 +63,10 @@ impl NixInstaller {
 
         if !extra_nix_config.contains("experimental-features") {
             config.push("experimental-features = nix-command flakes");
+        }
+
+        if self.os == OperatingSystem::Linux {
+            config.push("build-users-group = ");
         }
 
         let file = self
@@ -74,26 +82,128 @@ impl NixInstaller {
     }
 
     /// Assemble installer flags
-    fn assemble_installer_flags(&self) -> Result<(), anyhow::Error> {
+    pub fn assemble_installer_flags(&mut self) -> Result<(), anyhow::Error> {
+        let os = detect_os(&self.shell)?;
+        let workdir = &self
+            .workdir
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("workdir has not been created"))?
+            .path();
+        let config_file_flag = format!("--nix-extra-conf-file \"{}/nix.conf\"", workdir.display());
+        self.installer_options.push("--no-channel-add".to_string());
+        self.installer_options
+            .push("--darwin-use-unencrypted-nix-store-volume".to_string());
+        self.installer_options.push(config_file_flag);
+
+        match os {
+            OperatingSystem::MacOS | OperatingSystem::LinuxSystemD => {
+                let daemon_user_count = format!("--daemon-user-count {}", num_cpus::get() * 2);
+                self.installer_options.push("--daemon".to_string());
+                self.installer_options.push(daemon_user_count)
+            }
+            OperatingSystem::Linux => {
+                self.shell.create_dir("/etc/nix")?;
+                cmd!(self.shell, "cp {workdir}/nix.conf /etc/nix/nix.conf")
+                    .quiet()
+                    .run()?;
+            }
+        }
+
+        let mut input_install_options = self
+            .shell
+            .var("INPUT_INSTALL_OPTIONS")
+            .unwrap_or_else(|_| "".to_string())
+            .split(' ')
+            .map(|item| item.to_string())
+            .collect::<Vec<_>>();
+
+        input_install_options.append(&mut self.installer_options);
+        self.installer_options = input_install_options;
+
+        println!("installer options: {}", self.installer_options.join(" "));
+
         Ok(())
     }
 
     /// Download the installer
-    fn download_installer(&self) -> Result<(), anyhow::Error> {
+    pub fn download_installer(&self) -> Result<(), anyhow::Error> {
+        let installer_url = self
+            .shell
+            .var("INPUT_INSTALL_URL")
+            .unwrap_or_else(|_| "https://nixos.org/nix/install".to_string());
+
+        let response = reqwest::blocking::get(installer_url)?.text()?;
+
+        let installer_path = self
+            .workdir
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("workdir has not been created"))?
+            .path()
+            .join("installer");
+
+        self.shell.write_file(installer_path, response.as_bytes())?;
         Ok(())
     }
 
     /// Run the installer
-    fn run_installer(&self) -> Result<(), anyhow::Error> {
+    pub fn run_installer(&self) -> Result<(), anyhow::Error> {
+        let installer = self
+            .workdir
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("workdir has not been created"))?
+            .path()
+            .join("installer");
+        let options = self.installer_options.join(" ");
+        cmd!(self.shell, "sh {installer} {options}").quiet().run()?;
+        Ok(())
+    }
+
+    pub fn post_install_steps(&self) -> Result<(), anyhow::Error> {
+        if self.os == OperatingSystem::MacOS {
+            let cert_file = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+            cmd!(
+                self.shell,
+                "echo \"NIX_SSL_CERT_FILE={cert_file}\" >> \"$BASH_ENV\""
+            )
+            .run()?;
+            cmd!(self.shell, "export NIX_SSL_CERT_FILE={cert_file}").run()?;
+            cmd!(
+                self.shell,
+                "sudo launchctl setenv NIX_SSL_CERT_FILE \"{cert_file}\""
+            )
+            .run()?;
+        }
+
+        cmd!(
+            self.shell,
+            "export PATH=\"/nix/var/nix/profiles/per-user/$USER/profile/bin:/nix/var/nix/profiles/default/bin:$PATH\" >> \"BASH_ENV\""
+        )
+        .run()?;
+
+        let custom_path = self
+            .shell
+            .var("INPUT_NIX_PATH")
+            .unwrap_or_else(|_| "".to_string());
+
+        if !custom_path.is_empty() {
+            cmd!(self.shell, "\"NIX_PATH={custom_path}\" >> \"$BASH_ENV\"").run()?;
+        }
+
         Ok(())
     }
 
     /// Returns a reference to the held `Shell`
+    /// Useful for testing
+    #[allow(dead_code)]
+    #[doc(hidden)]
     fn shell(&self) -> &Shell {
         &self.shell
     }
 
     /// Returns a reference to the held `TempDir`
+    /// Useful for testing
+    #[allow(dead_code)]
+    #[doc(hidden)]
     fn workdir(&self) -> &Option<TempDir> {
         &self.workdir
     }
@@ -101,19 +211,25 @@ impl NixInstaller {
 
 fn main() -> Result<(), anyhow::Error> {
     let mut installer = NixInstaller::new()?;
-    let sh = Shell::new()?;
 
     installer.nix_not_installed()?;
 
+    // Create a tempdir
     installer.create_workdir()?;
 
-    // Create a tempdir
     // Write config file
-    // Assemble installer flags
-    // Download the installer
-    // Run the installer
+    installer.write_config()?;
 
-    let _ = detect_os(&sh)?;
+    // Assemble installer flags
+    installer.assemble_installer_flags()?;
+
+    // Download the installer
+    installer.download_installer()?;
+
+    // Run the installer
+    installer.run_installer()?;
+
+    installer.post_install_steps()?;
 
     Ok(())
 }
@@ -122,6 +238,9 @@ fn detect_os(sh: &Shell) -> Result<OperatingSystem, anyhow::Error> {
     let output = cmd!(sh, "uname").read()?;
 
     if output.contains("Linux") {
+        if cmd!(sh, "[ -e /run/systemd/system ]").quiet().run().is_ok() {
+            return Ok(OperatingSystem::LinuxSystemD);
+        }
         Ok(OperatingSystem::Linux)
     } else if output.contains("Darwin") {
         Ok(OperatingSystem::MacOS)
@@ -153,7 +272,7 @@ mod tests {
         let installer = setup();
         let os = detect_os(installer.shell());
         assert!(os.is_ok());
-        assert_eq!(os.unwrap(), OperatingSystem::Linux);
+        assert_eq!(os.unwrap(), OperatingSystem::LinuxSystemD);
     }
 
     #[test]
