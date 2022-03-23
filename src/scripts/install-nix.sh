3a0ba257a27fd5377cd258fcf94bf466b43f9d49
Eric Dattore <eric.dattore@gmail.com>
2022-03-23 14:50:47 -0600

    fix curl retries

--- src/scripts/install-nix.sh
+++ src/scripts/install-nix.sh
@@ -27,10 +27,19 @@ InstallNix() {
     fi
 
     tag_name="${BATS_TAG_NAME:-${TAG_NAME}}"
-    echo $tag_name
     download_url="https://github.com/ELD/nix-orb/releases/download/${tag_name}/install-nix-${target_triple}"
     mkdir -p "${HOME}/.bin/nix-orb"
-    curl -o "${HOME}/.bin/nix-orb/install-nix" -L --fail --retries 5 ${download_url} > /dev/null
+
+    local curl_retries
+    curl_retries=5
+    while ! curl -o "${HOME}/.bin/nix-orb/install-nix" -L --fail "${download_url}" > /dev/null; do
+        sleep 1
+        ((curl_retries--))
+        if [[ $curl_retries -le 0 ]]; then
+            echo "unable to download binary" >& 2
+            exit 1
+        fi
+    done
 
     if [[ "${BATS_TEST}" == "true" ]]; then
         echo "Would run install-nix on ${os}"
@@ -38,13 +47,13 @@ InstallNix() {
     fi
 
     # Close stdin so the installer runs non-interactively
-    $HOME/.bin/nix-orb/install-nix < /dev/null
+    "$HOME"/.bin/nix-orb/install-nix < /dev/null
 
     # shellcheck source=/dev/null
-    . $BASH_ENV
+    . "$BASH_ENV"
 }
 
 ORB_TEST_ENV="bats-core"
-if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+if [ "${0#*"$ORB_TEST_ENV"}" == "$0" ]; then
     InstallNix
 fi
